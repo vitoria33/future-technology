7872e7045f9dd22d22eb2468fcf14e699e6b79e5
Vitória Cristina do Nascimento <105313328+vitoria33@users.noreply.github.com>
2023-11-21 07:01:33 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-document.querySelector('')
+function addNewTask() {
+    var list = document.getElementById("paginas");
+    var text = document.getElementById("taskname").value;
+
+if(text.length === 0) {
+    alert("Tarefa precisa de ter mais de 1 caracter");
+    return;
+}
+
+    var listItem = document.createElement("li");
+    listItem.className ="list-Item";
+
+    const textElemnet = documenr.createTextNote(text);
+    listItem.appendChild(textElement)
+    list.appendChild(listItem)
+}
